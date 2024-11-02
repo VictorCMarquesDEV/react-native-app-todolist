ebb35923b29b1639d57f861b53ba2785b3f6893f
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-11-02 16:29:05 -0300

    update project

--- src/database/initializeDB.ts
+++ src/database/initializeDB.ts
@@ -1,14 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_URL, SUPABASE_ANONKEY } from '@env'
 
-const supabaseUrl = "https://npigntvaohmpbyqubhjf.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5waWdudHZhb2htcGJ5cXViaGpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5NjI2MzgsImV4cCI6MjA0NDUzODYzOH0.c8DtjsxjjSIZCdL9N4bjpENnp4d78IcHQzvDPVRFpJg"
+const supabaseUrl = SUPABASE_URL
+const supabaseAnonKey = SUPABASE_ANONKEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
 })
